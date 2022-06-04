2e93bf7d6b4a9b06dd4cab4fd1fe7b9958ca212d
Lunannam <lunkova02@gmail.com>
2022-06-05 01:31:55 +0300

    change example5_conditions

---
+++ example5_conditions/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя:");
+string unsername = Console.ReadLine();
+
+if(unsername.ToLower() == "Маша")
+{
+    Console.WriteLine("Ура! Это же Маша!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(unsername);
+}
